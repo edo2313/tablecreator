6d9dff24c784f7a8e329a715fb62204f81d86765
edo-2313 <32812884+edo2313@users.noreply.github.com>
2021-02-13 11:16:00 +0100

    fixed

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ function createTable(){
         if(column == 1)
             output += "<tr>";
         let link = value.toLowerCase().split(' ').join('-');
-        let name = value.split(' ').pop().toUpperCase().push();
+        value = value.split('');
+        let name =  value.push(value.pop().toUpperCase()).join(' ');
         output += '<td><a target="_blank" href="http://medyapro.it/'+link+'">'+name+'</a></td>';
         column++;
         if( column > 4) {
